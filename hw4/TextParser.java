4b44e737c4ab35594374e409b267b41876cb4a28
gleonidou17 <gleoni04@ucy.ac.cy>
2025-04-13 15:15:49 +0300

    Started TextParser.java

--- hw4/TextParser.java
+++ hw4/TextParser.java
@@ -1,6 +1,8 @@
 package hw4;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class TextParser implements TextOperations{
     private String text;
@@ -9,13 +11,19 @@ public class TextParser implements TextOperations{
         this.text = text;
     }       
 
+    private ArrayList<String> tokenizerToList(String delim){
+        StringTokenizer tokenizer = new StringTokenizer(this.text,delim);
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < tokenizer.countTokens(); i++) {
+            list.add(tokenizer.nextToken().toString().trim());
+        }
+        return list;       
+    }
+
     @Override
     public ArrayList<ArrayList<String>> getTextAsList(String text) {
-        /**
-         * code
-         */
         ArrayList<ArrayList<String>> list = new ArrayList<>();
-        return list;
+        
     }
 
     @Override

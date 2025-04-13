0468bf482855a126f8a460dbccc8f7fde06ef2e9
gleonidou17 <gleoni04@ucy.ac.cy>
2025-04-13 17:03:00 +0300

    Completed similarityCalculator.java

--- hw4/SimilarityCalculator.java
+++ hw4/SimilarityCalculator.java
@@ -3,18 +3,21 @@ package hw4;
 import java.util.HashMap;
 
 public class SimilarityCalculator {
-    public double calculateSimilarity(HashMap<String, Integer> vec1, HashMap<String, Integer> vec2) {
-        /**
-         * code
-         */ 
-        return 0;
-        //evala return 0 gia na min mou vazei error 
+    public double cosineSimilarity(HashMap<String, Integer> vec1, HashMap<String, Integer> vec2) {
+        double dotPoroduct = 0;
+        for (String key : vec1.keySet()) {
+            if (vec2.containsKey(key)) {
+                dotPoroduct += vec1.get(key) * vec2.get(key);
+            }
+        }
+        return dotPoroduct / (calculateNorm(vec1) * calculateNorm(vec2));
     }
     
-    private static double sigmaNotation(int startValue, int endValue, HashMap<String, Integer> vec1, HashMap<String, Integer> vec2, int parameters) {
-        /**
-         * code
-         */
-        return 0;
+    private static double calculateNorm(HashMap<String, Integer> vec) {
+        double norm = 0;
+        for (String key : vec.keySet()) {
+            norm += Math.pow(vec.get(key), 2);
+        }   
+        return Math.sqrt(norm);
     }
 }
